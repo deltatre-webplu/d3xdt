348e4e1eef57b60586c973bd91975f527adbe7e8
Davide Icardi <davide.icardi@gmail.com>
2015-07-24 17:34:54 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# .confg transformation tool
+
+Usage:
+
+d3xdt "c:\temp\web.config" "c:\temp\web.release.config" "c:\temp\release.config"
+
+See also:
+http://sedodream.com/2013/02/02/HowToInvokeXDTFromCode.aspx
+https://www.nuget.org/packages/Microsoft.Web.Xdt/
+
---
+++ d3xdt/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Web.XmlTransform;
+
+namespace d3xdt
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args == null || args.Length < 3)
+			{
+				ShowUsage();
+				return;
+			}
+
+			var sourceFile = args[0];
+			var transformFile = args[1];
+			var destFile = args[2] ?? sourceFile;
+
+			if (!File.Exists(sourceFile))
+			{
+				throw new FileNotFoundException("sourceFile doesn't exist");
+			}
+			if (!File.Exists(transformFile))
+			{
+				throw new FileNotFoundException("transformFile doesn't exist");
+			}
+
+			using (var document = new XmlTransformableDocument())
+			{
+				document.PreserveWhitespace = true;
+				document.Load(sourceFile);
+
+				using (var transform = new XmlTransformation(transformFile))
+				{
+					var success = transform.Apply(document);
+
+					document.Save(destFile);
+
+					Console.WriteLine("\nSaved transformation at '{0}'\n\n", new FileInfo(destFile).FullName);
+
+					var exitCode = (success) ? 0 : 1;
+					Environment.Exit(exitCode);
+				}
+			}
+		}
+
+		private static void ShowUsage()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("\n\nIncorrect set of arguments");
+			sb.AppendLine("\tXdtSample.exe sourceXmlFile transformFile destFile\n\n");
+			Console.Write(sb.ToString());
+		}
+	}
+}
